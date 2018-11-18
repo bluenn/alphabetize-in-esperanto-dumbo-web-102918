21179b2d614d1cde7d8c54e28ce4faacddbee72b
a lan <bluenowte97@gmail.com>
2018-11-18 20:48:47 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   # code here
   order = [a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z]
   arr.sort_by do |x|
-    order.index(x[0])
+    order.index x[0]
   end
   arr
 end
