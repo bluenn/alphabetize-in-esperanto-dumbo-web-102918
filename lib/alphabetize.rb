1398e242c961cbd6d2044133238869753c1df775
a lan <bluenowte97@gmail.com>
2018-11-18 21:00:40 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,7 @@ def alphabetize(arr)
   # code here
   order = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
   arr.sort_by do |element|
-    order.index(element[0][0])
+    order.index(element)
   end
   arr
 end
