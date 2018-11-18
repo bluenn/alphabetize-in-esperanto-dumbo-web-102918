f540ea843920a25cb407c9d7b8722f72d61ee638
a lan <bluenowte97@gmail.com>
2018-11-18 20:40:59 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
   # code here
-  Esperanto_order = [a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z]
+  order = [a,b,c,ĉ,d,e,f,g,ĝ,h,ĥ,i,j,ĵ,k,l,m,n,o,p,r,s,ŝ,t,u,ŭ,v,z]
   arr.sort_by do |x|
     order.index(x[0])
   end
